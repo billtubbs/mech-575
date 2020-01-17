8356b0f301666ad09d8e17e2c8b6e6c1783f81a7
billtubbs <bill.tubbs@me.com>
2020-01-17 13:54:00 -0800

    Renamed

--- code/python/code_1_9.py
+++ code/python/code_1_9.py
@@ -1,7 +1,9 @@
 from math import sin, cos, pi, sqrt
 import numpy as np
 import matplotlib.pyplot as plt
-#from sklearn.decomposition import PCA
+
+# Initialize random number generator
+rng = np.random.RandomState(1)
 
 theta = pi/3
 xC = np.array([2, 1]).reshape(-1, 1)
@@ -10,17 +12,12 @@ R = np.array([[cos(theta), -sin(theta)], [sin(theta), cos(theta)]])
 
 # Generate random data
 n = 10000
-X = np.dot(R*sig, np.random.randn(2, n)) \
-    + np.repeat(xC, n, axis=1)
+X = np.dot(R*sig, rng.randn(2, n)) + xC
 
 # Compute PCA via SVD
 Xavg = X.mean(axis=1)
 B = X - Xavg.reshape(-1, 1)
 U, S, VH = np.linalg.svd(B/sqrt(n), full_matrices=False)
-
-# How to compute using PCA
-#pca = PCA(n_components=2)
-#pca.fit(X)
 
 # Compute confidence intervals
 theta = np.linspace(0, 1, 100) * 2 * pi
@@ -28,18 +25,16 @@ circle_pts = np.stack([np.cos(theta), np.sin(theta)])
 Xstd = np.dot(U*S, circle_pts)
 
 # Plot figure
-plt.scatter(X[0,:], X[1,:], marker='.')
+plt.figure(figsize=(5, 5))
+plt.scatter(X[0,:], X[1,:], marker='.', alpha=0.2)
 plt.plot(Xavg[0] + Xstd[0, :], Xavg[1] + Xstd[1, :], color='r')
 plt.plot(Xavg[0] + 2*Xstd[0, :], Xavg[1] + 2*Xstd[1, :], color='r')
 plt.plot(Xavg[0] + 3*Xstd[0, :], Xavg[1] + 3*Xstd[1, :], color='r')
+plt.axis('equal')
 plt.xlabel('x')
 plt.ylabel('y')
 plt.grid()
+plt.tight_layout()
+plt.savefig('../../plots/fig_1_13a_py.pdf')
+plt.savefig('../../plots/fig_1_13a_py.png')
 plt.show()
-
-def draw_vector(v0, v1, ax=None):
-    ax = ax or plt.gca()
-    arrowprops=dict(arrowstyle='->',
-                    linewidth=2,
-                    shrinkA=0, shrinkB=0)
-    ax.annotate('', v1, v0, arrowprops=arrowprops)
