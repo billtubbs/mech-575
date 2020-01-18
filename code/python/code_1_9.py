7e1a3dac3e14c4e280888f9b3ab77f238497ecb9
billtubbs <bill.tubbs@me.com>
2020-01-18 13:18:24 -0800

    Modified to match book code

--- code/python/code_1_9.py
+++ code/python/code_1_9.py
@@ -14,22 +14,9 @@ R = np.array([[cos(theta), -sin(theta)], [sin(theta), cos(theta)]])
 n = 10000
 X = np.dot(R*sig, rng.randn(2, n)) + xC
 
-# Compute PCA via SVD
-Xavg = X.mean(axis=1)
-B = X - Xavg.reshape(-1, 1)
-U, S, VH = np.linalg.svd(B/sqrt(n), full_matrices=False)
-
-# Compute confidence intervals
-theta = np.linspace(0, 1, 100) * 2 * pi
-circle_pts = np.stack([np.cos(theta), np.sin(theta)])
-Xstd = np.dot(U*S, circle_pts)
-
 # Plot figure
 plt.figure(figsize=(5, 5))
 plt.scatter(X[0,:], X[1,:], marker='.', alpha=0.2)
-plt.plot(Xavg[0] + Xstd[0, :], Xavg[1] + Xstd[1, :], color='r')
-plt.plot(Xavg[0] + 2*Xstd[0, :], Xavg[1] + 2*Xstd[1, :], color='r')
-plt.plot(Xavg[0] + 3*Xstd[0, :], Xavg[1] + 3*Xstd[1, :], color='r')
 plt.axis('equal')
 plt.xlabel('x')
 plt.ylabel('y')
