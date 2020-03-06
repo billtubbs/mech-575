608fd3bc24e84b0b3a829a5d0f1c03f5bf180017
billtubbs <bill.tubbs@me.com>
2020-03-05 22:27:15 -0800

    First commit

---
+++ code/matlab/era_demo.m
@@ -0,0 +1,43 @@
+clear all
+close all
+load testSys
+
+%% Dsiplay full system info
+numInputs, numOutputs
+size(sysFull.A)
+size(sysFull.B)
+size(sysFull.C)
+size(sysFull.D)
+
+%% Plot impulse response of full system
+figure
+impulse(sysFull,0:1:(r*5)+1);
+grid;
+set(gcf,'position',[100,700,800,400]);
+saveas(gcf,"../../plots/era_sys_imp.png")
+
+%% Obtain impulse response of full system
+[yFull,t] = impulse(sysFull,0:1:(r*5)+1);  
+YY = permute(yFull,[2 3 1]); % Reorder to be size p x q x m 
+                             % (default is m x p x q)
+%% Compute ERA from impulse response
+mco = floor((length(yFull)-1)/2)
+[Ar,Br,Cr,Dr,HSVs] = ERA(YY,mco,mco,numInputs,numOutputs,r);
+sysERA = ss(Ar,Br,Cr,Dr,-1);
+
+%% Plot impulse responses for both models
+figure
+impulse(sysFull, sysERA, 50);
+grid
+legend('Full model, n=100',['ERA, r=',num2str(r)])
+set(gcf,'position',[100,400,800,400]);
+set(gcf,'PaperPositionMode','auto')
+saveas(gcf,'../../plots/era_sys2_imp.png')
+
+
+%% Compare Bode plots
+figure
+bode(sysFull, sysERA);
+set(gcf,'position',[100,100,800,400]);
+set(gcf,'PaperPositionMode','auto')
+saveas(gcf,'../../plots/era_sys2_bode.png')
